f1c6ebb0354f393698e9ee9693725ed87b07b1dd
mnmallea <mnmallea@gmail.com>
2019-11-20 00:22:03 -0300

    Removed unnecesary stuff

--- src/client/Trivium/cipher.js
+++ src/client/Trivium/cipher.js
@@ -1,36 +1,33 @@
-"use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const trivium = __importStar(require("./trivium"));
-const utils = __importStar(require("./utils"));
-function cipher(data, key, iv) {
-    if (key.length !== 10 || iv.length !== 10) {
-        throw new TypeError("Key and IV length should be 10");
-    }
-    const keyBitarray = utils.toBitarray(key);
-    const ivBitarray = utils.toBitarray(iv);
-    const state = trivium.initializeInternalState(keyBitarray, ivBitarray);
-    const cipherBuffer = Buffer.alloc(data.length);
-    for (let i = 0; i < data.length; i++) {
-        const dataByte = data.readUInt8(i);
-        const cipherByte = trivium.nextByte(state);
-        cipherBuffer.writeUInt8(cipherByte ^ dataByte, i);
-    }
-    return cipherBuffer;
+import * as trivium from "./trivium";
+import * as utils from "./utils";
+
+export function cipher(data, key, iv) {
+  if (key.length !== 10 || iv.length !== 10) {
+    throw new TypeError("Key and IV length should be 10");
+  }
+
+  const keyBitarray = utils.hexToBitArray(key);
+  const ivBitarray = utils.hexToBitArray(iv);
+
+  const state = trivium.initializeInternalState(keyBitarray, ivBitarray);
+
+  const cipherBuffer = Buffer.alloc(data.length);
+
+  for (let i = 0; i < data.length; i++) {
+    const dataByte = data.readUInt8(i);
+    const cipherByte = trivium.nextByte(state);
+    cipherBuffer.writeUInt8(cipherByte ^ dataByte, i);
+  }
+
+  return cipherBuffer;
 }
-exports.cipher = cipher;
-function cipherBmp(data, key, iv) {
-    const BMP_HEADER_SIZE = 54;
-    const header = data.slice(0, BMP_HEADER_SIZE);
-    const payload = data.slice(BMP_HEADER_SIZE);
-    const cipheredPayload = cipher(payload, key, iv);
-    return Buffer.concat([header, cipheredPayload]);
+
+export function cipherBmp(data, key, iv) {
+  const BMP_HEADER_SIZE = 54;
+  const header = data.slice(0, BMP_HEADER_SIZE);
+  const payload = data.slice(BMP_HEADER_SIZE);
+
+  const cipheredPayload = cipher(payload, key, iv);
+
+  return Buffer.concat([header, cipheredPayload]);
 }
-exports.cipherBmp = cipherBmp;
-//# sourceMappingURL=cipher.js.map
--- src/client/Trivium/utils.js
+++ src/client/Trivium/utils.js
@@ -1,16 +1,18 @@
-"use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const bitwise = __importStar(require("bitwise"));
-function toBitarray(string) {
-    const buffer = Buffer.from(string);
-    return bitwise.buffer.read(buffer);
+import * as bitwise from "bitwise";
+
+export function toBitarray(string) {
+  const buffer = Buffer.from(string);
+  return bitwise.buffer.read(buffer);
 }
-exports.toBitarray = toBitarray;
-//# sourceMappingURL=utils.js.map
+
+export function hexToAscii(string) {
+  let str = "";
+  for (let n = 0; n < string.length; n += 2) {
+    str += String.fromCharCode(parseInt(string.substr(n, 2), 16));
+  }
+  return str;
+}
+
+export function hexToBitArray(hexString) {
+  return toBitarray(hexToAscii(hexString));
+}
